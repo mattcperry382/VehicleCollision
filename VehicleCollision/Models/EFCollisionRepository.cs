4b530d3a6cc6590a8692c69ca032298afad906e2
mattcperry382 <mattcperry382@gmail.com>
2022-04-04 16:14:28 -0600

    Set up models and start to display data

---
+++ VehicleCollision/Models/EFCollisionRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleCollision.Models
+{
+    public class EFCollisionRepository : ICollisionRepository
+    {
+        private CollisionContext context { get; set; }
+        public EFCollisionRepository(CollisionContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Crash> Crashes => context.Crashes;
+
+        public void SaveCrash(Crash c)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateCrash(Crash c)
+        {
+            context.Add(c);
+            context.SaveChanges();
+        }
+
+        public void DeleteCrash(Crash c)
+        {
+            context.Remove(c);
+            context.SaveChanges();
+        }
+    }
+}
