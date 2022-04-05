2442a7c47e62c1ef4a96059b6e73b522a7d354f5
Jacob D <jwdonaldson99@gmail.com>
2022-04-05 12:59:18 -0600

    Added in most auth stuff. Register user working Bootstrap 5.1 Moved data view page

---
+++ VehicleCollision/Services/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace VehicleCollision.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            SmtpClient client = new SmtpClient
+            {
+                Port = 587,
+                Host = "smtp-relay.sendinblue.com", //or another email sender provider
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("providentia29@gmail.com", "Dw&Y$HJJ64@fMsF")
+            };
+
+            return client.SendMailAsync("providentia29@gmail.com", email, subject, htmlMessage);
+        }
+    }
+}
--- VehicleCollision/Startup.cs
+++ VehicleCollision/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VehicleCollision.Models;
+using VehicleCollision.Services;
 
 namespace VehicleCollision
 {
@@ -26,12 +28,13 @@ namespace VehicleCollision
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<IdentityContext>(options =>
                 options.UseMySql(Configuration["ConnectionStrings:IdentityDBConnection"]));
             services.AddDbContext<CollisionContext>(options =>
                 options.UseMySql(Configuration["ConnectionStrings:CollisionDBConnection"]));
             services.AddScoped<ICollisionRepository, EFCollisionRepository>();
-          
+
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
@@ -44,18 +47,32 @@ namespace VehicleCollision
                 options.User.RequireUniqueEmail = true;
 
 
-            }).AddEntityFrameworkStores<IdentityContext>();
+            }).AddEntityFrameworkStores<IdentityContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddAuthentication();
+
 
             services.AddFido(options =>
             {
-            options.Licensee = "DEMO";
-            options.LicenseKey =
-                "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsIlJlbmV3YWxTZW50VGltZSI6IjAwMDEtMDEtMDFUMDA6MDA6MDAiLCJhdXRoIjoiREVNTyIsImV4cCI6IjIwMjItMDUtMDRUMDE6MDA6MDMuNTMzNDU5OSswMDowMCIsImlhdCI6IjIwMjItMDQtMDRUMDE6MDA6MDMiLCJvcmciOiJERU1PIiwiYXVkIjo2fQ==.fKuw59Ym4xwWB6dSYpfENPLblFROIzjj6P5LehisgGVioN+9H1K6wKdiP5aIHuJgLgVbx02emmSK9E4navvKR4/SxXabY1ebMD8uTzqfzfsPZA8zPONiH6qYwdciSIPpdNOQEbYjdJgRmECyfj3P5pxZjSYaqQoJacKf1ex30ULOXVLopi656kNKB3EIK5Pbvs+nNM97hfbBXLTFsvlAjsMABbQ4gZ4PCFTjTlsQxolze7CYfZTv0JUBmdIsfvQ1KpHvXFCogQbQVIT8sSPUaZjLfJZycnkMK/K9PWvedcmHUDVb7RK39W6O0XWRLjwDJLRwTAUVt0lsQJutO1gSMlbYoLC3L4fU5sUscu0cFhHm39Fe9AnN3ltDu/x0yyjRNzdghSdFC+1xz5Oo1ZBXkEc6PCX47KQ44jWvffUWsf2jLeR9LeUeKEQWoEX/J9gtKPtjxyl0WHo0NDf0CkauaMEQ1nPqH65CmwqeSKSvk1r0w7im2a5JWknM3kt6EPRJ5YIca8k4U2ONiyND0paUBcN+40cQEOJlmplLYS7r8jt6LRTNUrrOtY0EZ1w5A1ZXF5vNFx48wn+r1vURGcqGbqzdTHmePd2hkOFK7h8+vloqbNZ5XvoD+I1bkm+oFqZcRdcE0xW+f1hKAE/QpvFsZPY+M37bXjPo1Hob1llKT2o=";
-        }).AddInMemoryKeyStore();
+                options.Licensee = "DEMO";
+                options.LicenseKey =
+                    "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsIlJlbmV3YWxTZW50VGltZSI6IjAwMDEtMDEtMDFUMDA6MDA6MDAiLCJhdXRoIjoiREVNTyIsImV4cCI6IjIwMjItMDUtMDRUMDE6MDA6MDMuNTMzNDU5OSswMDowMCIsImlhdCI6IjIwMjItMDQtMDRUMDE6MDA6MDMiLCJvcmciOiJERU1PIiwiYXVkIjo2fQ==.fKuw59Ym4xwWB6dSYpfENPLblFROIzjj6P5LehisgGVioN+9H1K6wKdiP5aIHuJgLgVbx02emmSK9E4navvKR4/SxXabY1ebMD8uTzqfzfsPZA8zPONiH6qYwdciSIPpdNOQEbYjdJgRmECyfj3P5pxZjSYaqQoJacKf1ex30ULOXVLopi656kNKB3EIK5Pbvs+nNM97hfbBXLTFsvlAjsMABbQ4gZ4PCFTjTlsQxolze7CYfZTv0JUBmdIsfvQ1KpHvXFCogQbQVIT8sSPUaZjLfJZycnkMK/K9PWvedcmHUDVb7RK39W6O0XWRLjwDJLRwTAUVt0lsQJutO1gSMlbYoLC3L4fU5sUscu0cFhHm39Fe9AnN3ltDu/x0yyjRNzdghSdFC+1xz5Oo1ZBXkEc6PCX47KQ44jWvffUWsf2jLeR9LeUeKEQWoEX/J9gtKPtjxyl0WHo0NDf0CkauaMEQ1nPqH65CmwqeSKSvk1r0w7im2a5JWknM3kt6EPRJ5YIca8k4U2ONiyND0paUBcN+40cQEOJlmplLYS7r8jt6LRTNUrrOtY0EZ1w5A1ZXF5vNFx48wn+r1vURGcqGbqzdTHmePd2hkOFK7h8+vloqbNZ5XvoD+I1bkm+oFqZcRdcE0xW+f1hKAE/QpvFsZPY+M37bXjPo1Hob1llKT2o=";
+            }).AddInMemoryKeyStore();
+                //.AddEntityFrameworkStore(options =>
+                //options.UseMySql(Configuration["ConnectionStrings:IdentityDBConnection"]));
 
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddTransient<IEmailSender, EmailSender>();
+            //services.AddTransient<IEmailSender, YourSmsSender>();
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +88,7 @@ namespace VehicleCollision
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+          
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -79,6 +97,19 @@ namespace VehicleCollision
             //authentication and Identity
             app.UseAuthentication();
             app.UseAuthorization();
+
+            //X-Xss Protection
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                await next();
+            });
+
+            app.Use(async (ctx, next) =>
+            {
+                ctx.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; font-src 'self' data: fonts.gstatic.com;style-src 'self' fonts.googleapis.com; img-src 'self' data:") ;
+                await next();
+            });
 
             app.UseEndpoints(endpoints =>
             {
