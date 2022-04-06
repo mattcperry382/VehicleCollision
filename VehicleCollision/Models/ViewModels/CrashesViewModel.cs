f6cfa4b7f953b111760c53b9e0da737d15c9df35
jessi472 <79349080+jessi472@users.noreply.github.com>
2022-04-06 16:01:28 -0600

    Pagination and associated stuff

---
+++ VehicleCollision/Models/ViewModels/CrashesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleCollision.Models.ViewModels
+{
+    public class CrashesViewModel
+    {
+        public IQueryable<Crash> Crashes { get; set; }
+
+        public IQueryable<CrashSeverity> CrashSeverity { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
